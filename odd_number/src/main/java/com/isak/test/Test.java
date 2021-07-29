4bfa2b6272583360b5fd8ef9f8a40839d8506803
林雨华 <1503386669@qq.com>
2021-07-29 21:45:40 +0800

    测试右移运算

--- odd_number/src/main/java/com/isak/test/Test.java
+++ odd_number/src/main/java/com/isak/test/Test.java
@@ -11,5 +11,26 @@ public class Test {
             result = result ^ i;
         }
         System.out.println(result);
+
+        // 求i和j的中间数  int的范围为-(2^31)~(2^31-1) --->  -2147483648~2147483647
+        System.out.println(getMidle1(2147483600,2147483647));
+        System.out.println(getMidle2(2147483600,2147483647));
+        System.out.println(getMidle3(2147483600,2147483647));
+
+    }
+
+    // 方法一   该方法在数据较大的时候容易出错
+    private static int getMidle1(int a, int b) {
+        return (a + b) / 2;
+    }
+
+    // 方法二    该方法不会出错，但是效率没有方法三快
+    private static int getMidle2(int a, int b) {
+        return a + (b - a) / 2;
+    }
+
+    // 方法三    安全快速
+    private static int getMidle3(int a, int b) {
+        return a + ((b - a) >> 1);
     }
 }
